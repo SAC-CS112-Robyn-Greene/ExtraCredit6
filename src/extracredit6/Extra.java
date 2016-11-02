78311af37fd7818a43ecb09c6d8b610dafa25d9e
Robyn <Robyn@DESKTOP-Q53AP1R>
2016-11-01 18:30:39 -0700

    Part 6 functions extra credit Tried to do a constructor and method with names

---
+++ src/extracredit6/Extra.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package extracredit6;
+
+/**
+ *
+ * @author Robyn
+ */
+import java.util.*;
+import java.util.Random;
+public class Extra {
+    
+  private String name;//instance variable
+
+
+//constructor
+public Extra(String name)
+{
+this.name=name;
+
+}
+    
+   public void setName(String name)
+   {
+   this.name=name; 
+   }
+   
+    public String getName()
+   {
+       return name;
+   }
+    
+    
+}//class
